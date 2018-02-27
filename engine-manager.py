67d9ead78a05e14ccbe7b515ab254be4c7a0cf8a
Alexander Bondarev <alexander.bondarev@clarivate.com>
2018-02-27 17:21:15 +0300

    - Use threaded HTTP server for correct processing for multiple queries - Make all operations exclusive - Add chunked response support - Add commands for update-with-data and init-with-data - Refactor commands to be reusable

--- engine-manager.py
+++ engine-manager.py
@@ -32,38 +32,70 @@ class EngineManagerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     protocol_version = 'HTTP/1.1'
     server_version = 'PredictionIOEngineManager/' + __version__
     deploy_process = None
+    handling = False
 
     def do_GET(self):
         parts = urlparse.urlsplit(self.path)
         if parts.path == '/apps':
             self.send_command_output(["pio", "app", "list"])
             return
+        self.consume_and_ignore_request_body()
         self.send_not_found()
 
     def do_POST(self):
+        if EngineManagerHandler.handling:
+            self.consume_and_ignore_request_body()
+            return self.send_error(409, "Another operation in progress")
+
+        EngineManagerHandler.handling = True
         try:
             parts = urlparse.urlsplit(self.path)
             self.log_message("Processing command: %s...", parts.path)
             if parts.path == '/app/deploy':
-                if EngineManagerHandler.deploy_process:
-                    self.log_message("Killing existing process: %d", EngineManagerHandler.deploy_process.pid)
-                    subprocess.check_call(['pkill', '-TERM', '-P', str(EngineManagerHandler.deploy_process.pid)])
-                    EngineManagerHandler.deploy_process = None
-                EngineManagerHandler.deploy_process = subprocess.Popen(["pio", "deploy"])
-                return self.send_content("Deploy initiated")
+                return self.send_content(self.deploy())
             elif parts.path == "/app/train":
-                return self.send_command_output(
-                    ["pio", "train", "--", "--driver-memory", driver_memory, "--executor-memory", executor_memory])
+                return self.send_content(self.train())
             elif parts.path == "/app/import":
-                return self.handle_import_data()
+                return self.send_content(self.handle_import_data())
             elif parts.path == "/app/data-delete":
-                return self.send_command_output(["pio", "app", "data-delete", app_name, "--force"])
+                return self.send_content(self.data_delete())
+            elif parts.path == "/app/update-with-data" or parts.path == "/app/init-with-data":
+                self.start_chunked_response()
+                try:
+                    if parts.path == "/app/init-with-data":
+                        self.write_chunk("Cleanup...")
+                        self.write_chunk(self.data_delete())
+                    self.write_chunk("Import...")
+                    self.write_chunk(self.handle_import_data())
+                    self.write_chunk("Train...")
+                    self.write_chunk(self.train())
+                    self.write_chunk("Deploy...")
+                    self.write_chunk(self.deploy())
+                finally:
+                    self.end_chunked_response()
+                return
         except StandardError as ex:
             traceback.print_exc()
-            self.send_server_error(ex.message)
-            return
+            return self.send_server_error(ex.message)
+        finally:
+            EngineManagerHandler.handling = False
 
+        self.consume_and_ignore_request_body()
         self.send_not_found()
+
+    def data_delete(self):
+        return self.pio(["app", "data-delete", app_name, "--force"])
+
+    def deploy(self):
+        if EngineManagerHandler.deploy_process:
+            self.log_message("Killing existing process: %d", EngineManagerHandler.deploy_process.pid)
+            subprocess.check_call(['pkill', '-TERM', '-P', str(EngineManagerHandler.deploy_process.pid)])
+            EngineManagerHandler.deploy_process = None
+        EngineManagerHandler.deploy_process = subprocess.Popen(["pio", "deploy"])
+        return "Deploy initiated"
+
+    def train(self):
+        return self.pio(["train", "--", "--driver-memory", driver_memory, "--executor-memory", executor_memory])
 
     def copy_chunked_body(self, dest):
         """
@@ -88,19 +120,17 @@ class EngineManagerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             if chunk_size == 0:
                 break
 
+    @staticmethod
+    def pio(cmd):
+        return subprocess.check_output(["pio"] + cmd, cwd=work_dir)
+
     def handle_import_data(self):
         tmp_file_name = tempfile.mktemp()
         try:
             with open(tmp_file_name, 'wb') as tmp:
-                content_length = int(self.headers.get('content-length', '-1'))
-                if self.headers.get('transfer-encoding') == "chunked":
-                    self.copy_chunked_body(tmp)
-                elif content_length >= 0:
-                    tmp.write(self.rfile.read(content_length))
-                else:
-                    self.send_server_error("Can't determine content-length")
+                self.consume_request_body(tmp)
 
-            self.send_command_output(["pio", "import", "--appid", app_id, "--input", tmp_file_name])
+            return self.pio(["import", "--appid", app_id, "--input", tmp_file_name])
         finally:
             os.unlink(tmp_file_name)
 
@@ -132,9 +162,41 @@ class EngineManagerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         finally:
             f.close()
 
+    def start_chunked_response(self, mime_type="text/html", status_code=200):
+        self.send_response(status_code)
+        encoding = sys.getfilesystemencoding()
+        self.send_header("Content-Type", "%s; charset=%s" % (mime_type, encoding))
+        self.send_header("Transfer-Encoding", "chunked")
+        self.end_headers()
+
+    def write_chunk(self, chunk):
+        self.wfile.write("%x\r\n" % len(chunk))
+        self.wfile.write(chunk)
+        self.wfile.write("\r\n")
+
+    def end_chunked_response(self):
+        self.wfile.write("0\r\n\r\n")
+
+    def consume_and_ignore_request_body(self):
+        with open(os.devnull, "w") as null:
+            self.consume_request_body(null)
+
+    def consume_request_body(self, out):
+        content_length = int(self.headers.get('content-length', '-1'))
+        if self.headers.get('transfer-encoding') == "chunked":
+            self.copy_chunked_body(out)
+        elif content_length >= 0:
+            out.write(self.rfile.read(content_length))
+        else:
+            raise EOFError("Can't determine content-length")
+
+
+class ThreadedHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    """Handle requests in a separate thread."""
+
 
 if __name__ == '__main__':
-    httpd = SocketServer.TCPServer((ip, port), EngineManagerHandler)
+    httpd = ThreadedHTTPServer((ip, port), EngineManagerHandler)
 
     sys.stderr.write("Trying to deploy engine... It is OK to fail on this stage\n")
     EngineManagerHandler.deploy_process = subprocess.Popen(["pio", "deploy"])
